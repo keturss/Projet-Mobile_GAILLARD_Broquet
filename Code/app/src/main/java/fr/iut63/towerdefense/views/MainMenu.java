b02139b729e93bdcaf4142bc88862736970d094f
vigaillard2 <victor.gaillard@etu.uca.fr>
2022-02-05 16:16:48 +0100

    Création d'un nouveau projet + déplacement du projet dans Code/

---
+++ Code/app/src/main/java/fr/iut63/towerdefense/views/MainMenu.java
@@ -0,0 +1,19 @@
+package fr.iut63.towerdefense.views;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import fr.iut63.towerdefense.R;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+    }
+
+
+}
