2565bf2654123567ae3cf014ba07f94ab279b8e8
mabroquet1 <matteo.broquet@etu.uca.fr>
2022-02-08 09:57:50 +0100

    image

--- Code/app/src/main/java/fr/iut63/towerdefense/views/MainMenu.java
+++ Code/app/src/main/java/fr/iut63/towerdefense/views/MainMenu.java
@@ -6,6 +6,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import fr.iut63.towerdefense.R;
+import fr.iut63.towerdefense.model.gamelogic.GameManager;
+import fr.iut63.towerdefense.model.gamelogic.map.ImportMap;
+import fr.iut63.towerdefense.model.gamelogic.map.Map;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -13,6 +16,8 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+
+        //GameManager game = new GameManager("Keturss", new ImportMap(1216, 608));
     }
 
 
