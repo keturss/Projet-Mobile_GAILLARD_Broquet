02c023ac7ce0bd833e653916d9e8144c751e785a
vigaillard2 <victor.gaillard@etu.uca.fr>
2022-02-08 17:29:56 +0100

    Ajout de nouvelle vue + début de navigation depuis le menu principal - Création de la vue des paramètres - Ajout de la navigation sur les boutons paramètres & quitter

--- Code/app/src/main/java/fr/iut63/towerdefense/views/MainMenu.java
+++ Code/app/src/main/java/fr/iut63/towerdefense/views/MainMenu.java
@@ -1,6 +1,10 @@
 package fr.iut63.towerdefense.views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,8 +21,65 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
 
-        //GameManager game = new GameManager("Keturss", new ImportMap(1216, 608));
+
+        Button buttonQuit = (Button) findViewById(R.id.menu_buttonQuit);
+        buttonQuit.setOnClickListener(this::onClickQuit);
+
+        Button buttonParameters = (Button) findViewById(R.id.menu_buttonParam);
+        buttonParameters.setOnClickListener(this::onClickParam);
+
+        Log.d("testsLifecycle", "onCreate");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
 
+
+        Log.d("testsLifecycle", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.d("testsLifecycle", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("testsLifecycle", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("testsLifecycle", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("testsLifecycle", "onDestroy");
+    }
+
+    /**
+     * Méthode appelée lors du click du bouton quitter
+     * Quitte l'application
+     * @param view Bouton clické
+     */
+    public void onClickQuit(View view) {
+        finish();
+    }
+
+    /**
+     * Méthode appelée lors du click sur le bouton paramètres
+     * Envoie sur la vur des paramètres
+     * @param view Bouton clické
+     */
+    private void onClickParam(View view) {
+        Intent intent = new Intent(this, ParametersMenu.class);
+        startActivity(intent);
+    }
 }
--- README.md
+++
@@ -1,3 +0,0 @@
-# Projet-Mobile_GAILLARD_Broquet
-
-Projet de portage d'un jeu JavaFx en Android
